7f21afd6aa46a7c7456749c6408588c36ce303ea
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-07-25 12:12:06 +0200

    upgrade rodio to 0.21.1

--- src/notifs/audio.rs
+++ src/notifs/audio.rs
@@ -1,6 +1,7 @@
 use crate::domain::ChangeKind;
 use anyhow::Context;
-use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink};
+use rodio::mixer::Mixer;
+use rodio::{Decoder, OutputStream, OutputStreamBuilder, Sink};
 use std::io::Cursor;
 use tracing::error;
 
@@ -10,18 +11,16 @@ const REMOVE_SOUND: &[u8] = include_bytes!("assets/remove.wav");
 const ERROR_SOUND: &[u8] = include_bytes!("assets/error.wav");
 
 pub struct AudioPlayer {
-    _stream: OutputStream,
-    stream_handle: OutputStreamHandle,
+    stream: OutputStream,
 }
 
 impl AudioPlayer {
     pub fn new() -> anyhow::Result<Self> {
-        let (stream, stream_handle) = OutputStream::try_default()
-            .context("couldn't open stream to default output audio device")?;
-        Ok(AudioPlayer {
-            _stream: stream,
-            stream_handle,
-        })
+        let mut stream = OutputStreamBuilder::open_default_stream()
+            .context("couldn't open default output stream")?;
+        stream.log_on_drop(false);
+
+        Ok(AudioPlayer { stream })
     }
 
     pub fn play_change_sound(&self, change_kind: &ChangeKind) {
@@ -39,11 +38,11 @@ impl AudioPlayer {
     }
 
     fn play_sound(&self, sound_data: &'static [u8]) {
-        let stream_handle = self.stream_handle.clone();
-
         if tokio::runtime::Handle::try_current().is_ok() {
+            let mixer = self.stream.mixer().clone();
+
             tokio::task::spawn_blocking(move || {
-                if let Err(e) = try_playing_sound(sound_data, &stream_handle) {
+                if let Err(e) = try_playing_sound(sound_data, &mixer) {
                     error!("couldn't play sound: {e}");
                 }
             });
@@ -51,11 +50,8 @@ impl AudioPlayer {
     }
 }
 
-fn try_playing_sound(
-    sound_data: &'static [u8],
-    stream_handle: &OutputStreamHandle,
-) -> anyhow::Result<()> {
-    let sink = Sink::try_new(stream_handle)?;
+fn try_playing_sound(sound_data: &'static [u8], mixer: &Mixer) -> anyhow::Result<()> {
+    let sink = Sink::connect_new(mixer);
     let cursor = Cursor::new(sound_data);
     let source = Decoder::new(cursor)?;
 
